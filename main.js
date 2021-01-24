e4eddce18b4ae5d7e7d680ddd65d290d03c12707
Viktoras Bezaras <mail@viktoras.de>
2021-01-24 20:12:25 +0100

    Added second day solution and a linter

--- main.js
+++
@@ -1,4 +0,0 @@
-import d1p1 from './day1/solution.js';
-
-console.log('Part 1: ' + d1p1.part1());
-console.log('Part 2: ' + d1p1.part2());
---
+++ public/main.js
@@ -0,0 +1,8 @@
+import day1 from '../src/day1/solution.js';
+import day2 from '../src/day2/solution.js';
+
+console.log('Day 1 Part 1: ' + day1.part1());
+console.log('Day 1 Part 2: ' + day1.part2());
+
+console.log('Day 2 Part 1: ' + day2.part1());
+console.log('Day 2 Part 2: ' + day2.part2());
